0e0c39ff3fe4feec0b8fe29714f0c269b4e8e593
Romain2 <bonnenfant.romain@gmail.com>
2019-03-29 17:05:49 +0100

    Add fin endDay

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,26 +1,3 @@
-// Function to send email via smtp
-let myForm = document.getElementById('formMail');
-  myForm.addEventListener('submit', function(e)
-{
-    e.preventDefault();
-    let fullName = document.getElementById('fullName').value;
-    let mail = document.getElementById('mail').value;
-    let theSubject = document.getElementById('subject').value;
-    let theMessage = document.getElementById('message').value;
-
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "bonnenfant.romain@gmail.com",
-        Password : "87278544-595b-4dcd-b1bd-762a77ded37e",
-        To : "bonnenfant.romain@gmail.com",
-        From : mail,
-        Subject : theSubject,
-        Body : fullName+"_"+theSubject+" : "+theMessage
-    }).then(
-        message => alert(message)
-    );
-});
-
 // Cookies consent modal
 // function getCookie(cname) {
 //     const name = cname + "=";
