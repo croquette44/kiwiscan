77fa7a8263760023774a364f396728d7badb2113
remisimaer <remi.simaer@gmail.com>
2019-03-29 16:58:24 +0100

    multi-language class created, to finish next time

--- docs/js/lang.js
+++ docs/js/lang.js
@@ -1,9 +1,22 @@
+import { easyHTTP } from "./easyhttp.js";
+
 class Language {
     constructor(){
-        this.en = en;
-        this.fr = fr;
-        this.state = state;
+        // this.en = en;
+        // this.fr = fr;
+        // this.currentLang = currentLang;
+    }
+
+    getLang(){
+        console.log("object");
+
+        easyHTTP.get('./js/fr.json')
+            .then(data => console.log(data))
+            .catch(err => console.log(err));
+
     }
 
     
 }
+
+export const lang = new Language;
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,45 +1,56 @@
-let myForm = document.getElementById('formMail');
+import { lang } from "./lang.js";
 
-myForm.addEventListener('submit', function (e) {
-    e.preventDefault();
-    let fullName = document.getElementById('fullName').value;
-    let mail = document.getElementById('mail').value;
-    let subject = document.getElementById('subject').value;
-    let message = document.getElementById('message').value;
-    alert(`
-        ${fullName} , your mail is send !
-        for this : ${subject}.
-        your mail are : ${mail}.
-        your text : ${message}.
-        `);
+
+document.addEventListener('DOMContentLoaded', () => {
+    lang.getLang();
 });
 
-// Cookies consent modal
-function getCookie(cname) {
-    const name = cname + "=";
-    const ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1);
-        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
-    }
-    return "";
-}
+    // let myForm = document.getElementById('formMail');
 
-function createAcceptCookie() {
-    const cookieBanner = document.getElementById("cookie-banner");
-    document.cookie = "accepted_cookie_policy=yes; expires=" + setExpiryDate() + ";";
-    cookieBanner.classList.add("accepted-cookie-policy");
-}
+    // myForm.addEventListener('submit', function (e) {
+    //     e.preventDefault();
+    //     let fullName = document.getElementById('fullName').value;
+    //     let mail = document.getElementById('mail').value;
+    //     let subject = document.getElementById('subject').value;
+    //     let message = document.getElementById('message').value;
+    //     alert(`
+    //         ${fullName} , your mail is send !
+    //         for this : ${subject}.
+    //         your mail are : ${mail}.
+    //         your text : ${message}.
+    //         `);
+    // });
+    
 
-function setExpiryDate() {
-    const date = new Date();
-    date.setFullYear(date.getFullYear() + 1);
-    return date.toUTCString();
-}
-const isCookieSet = getCookie("accepted_cookie_policy");
-if (isCookieSet === "") {
-    document.getElementById("cookie-button").addEventListener("click", createAcceptCookie);
-} else {
-    document.getElementById("cookie-banner").classList.add("accepted");
-}
+    // // Cookies consent modal
+    // function getCookie(cname) {
+    //     const name = cname + "=";
+    //     const ca = document.cookie.split(';');
+    //     for (var i = 0; i < ca.length; i++) {
+    //         var c = ca[i];
+    //         while (c.charAt(0) == ' ') c = c.substring(1);
+    //         if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    //     }
+    //     return "";
+    // }
+    
+    // function createAcceptCookie() {
+    //     const cookieBanner = document.getElementById("cookie-banner");
+    //     document.cookie = "accepted_cookie_policy=yes; expires=" + setExpiryDate() + ";";
+    //     cookieBanner.classList.add("accepted-cookie-policy");
+    // }
+    
+    // function setExpiryDate() {
+    //     const date = new Date();
+    //     date.setFullYear(date.getFullYear() + 1);
+    //     return date.toUTCString();
+    // }
+
+
+    // const isCookieSet = getCookie("accepted_cookie_policy");
+
+    // if (isCookieSet === "") {
+    //     document.getElementById("cookie-button").addEventListener("click", createAcceptCookie);
+    // } else {
+    //     document.getElementById("cookie-banner").classList.add("accepted");
+    // }
