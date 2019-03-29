a1855dee66f7292c3a4e1e77912575e3f8b7cc5f
Romain2 <bonnenfant.romain@gmail.com>
2019-03-29 15:56:56 +0100

    email via smtp

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,17 +1,23 @@
+// Function to send email via smtp
 let myForm = document.getElementById('formMail');
-  
-myForm.addEventListener('submit', function(e)
+  myForm.addEventListener('submit', function(e)
 {
     e.preventDefault();
     let fullName = document.getElementById('fullName').value;
     let mail = document.getElementById('mail').value;
-    let subject = document.getElementById('subject').value;
-    let message = document.getElementById('message').value;
-    alert(`
-        ${fullName} , your mail is send !
-        for this : ${subject}.
-        your mail are : ${mail}.
-        your text : ${message}.
-        `);
-    });
+    let theSubject = document.getElementById('subject').value;
+    let theMessage = document.getElementById('message').value;
+
+        Email.send({
+            Host : "smtp.elasticemail.com",
+            Username : "bonnenfant.romain@gmail.com",
+            Password : "87278544-595b-4dcd-b1bd-762a77ded37e",
+            To : "bonnenfant.romain@gmail.com",
+            From : mail,
+            Subject : theSubject,
+            Body : fullName+"_"+theSubject+" : "+theMessage
+        }).then(
+          message => alert(message)
+        );
+});
    
